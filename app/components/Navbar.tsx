de64e8e1e4e87a5730e35c085320ad1e4f7af411
Jack Smith <jackesmith00@gmail.com>
2024-06-02 18:08:19 +0200

    Added hamburger menu functionality

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,85 +1,162 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import styles from "../styles";
-import { navVariants } from "../utils/motion";
+import { navVariants, mobileMenuVariants } from "../utils/motion";
 import Link from "next/link";
 import Image from "next/image";
-import menuBarSvg from "../../public/three-horizontal-lines-icon.svg"
+import menuBarSvg from "../../public/three-horizontal-lines-icon.svg";
+import { useState } from "react";
 
+const Navbar = () => {
+    const [isOpen, toggleIsOpen] = useState(false);
 
-const Navbar = () => (
-    <motion.nav
-        variants={navVariants}
-        initial="hidden"
-        whileInView="show"
-        className={`${styles.xPaddings} py-8 relative`}
-    >
-        <div className="absolute w-[50%] inset-0 gradient-01 -z-10" />
-        <div
-            className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}
+    const handleMenuClick = () => {
+        toggleIsOpen(!isOpen);
+        console.log(isOpen);
+    };
+
+    return (
+        <motion.nav
+            variants={navVariants}
+            initial="hidden"
+            whileInView="show"
+            className={`${styles.xPaddings} py-8 sm:pr-8 relative`}
         >
-            <h2 className="font-extrabold text-[24px] leading-[30.24px] text-white uppercase">
-                Neuroplay Lab
-            </h2>
-
-            <div className="items-center gap-8 hidden lg:flex">
-                <Link
-                    href="/"
-                    className="text-lg text-white hover:text-gray-300"
-                >
-                    Home
-                </Link>
-                <Link
-                    href="/#about-us"
-                    scroll
-                    className="text-lg text-white hover:text-gray-300"
-                >
-                    About Us
-                </Link>
-                <Link
-                    href="/contact"
-                    className="text-lg text-white hover:text-gray-300"
-                >
-                    Contact
-                </Link>
-                <Link
-                    href="/press"
-                    className="text-lg text-white hover:text-gray-300"
-                >
-                    Press
-                </Link>
-                <Link
-                    href="/publications"
-                    className="text-lg text-white hover:text-gray-300"
-                >
-                    Publications
-                </Link>
-                <Link
-                    href="/measures-and-code"
-                    className="text-lg text-white hover:text-gray-300"
-                >
-                    Measures and Code
-                </Link>
-                <Link
-                    href="/reasearch-topics"
-                    className="text-lg text-white hover:text-gray-300"
-                >
-                    Research Topics
-                </Link>
+            <div className="absolute w-[50%] inset-0 gradient-01 -z-10" />
+            <div
+                className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}
+            >
+                <h2 className="font-extrabold text-[24px] leading-[30.24px] text-white uppercase">
+                    Neuroplay Lab
+                </h2>
+                <div className="flex items-center gap-8 md:show lg:hidden">
+                    <button onClick={handleMenuClick}>
+                        <Image
+                            src={menuBarSvg}
+                            alt="menu"
+                            width={24}
+                            height={24}
+                            className="w-[24px] h-[24px] object-contain"
+                        />
+                    </button>
+                </div>
+                <div className="items-center gap-8 hidden lg:flex">
+                    <Link
+                        href="/"
+                        className="text-lg text-white hover:text-gray-300"
+                    >
+                        Home
+                    </Link>
+                    <Link
+                        href="/#about-us"
+                        scroll
+                        className="text-lg text-white hover:text-gray-300"
+                    >
+                        About Us
+                    </Link>
+                    <Link
+                        href="/contact"
+                        className="text-lg text-white hover:text-gray-300"
+                    >
+                        Contact
+                    </Link>
+                    <Link
+                        href="/press"
+                        className="text-lg text-white hover:text-gray-300"
+                    >
+                        Press
+                    </Link>
+                    <Link
+                        href="/publications"
+                        className="text-lg text-white hover:text-gray-300"
+                    >
+                        Publications
+                    </Link>
+                    <Link
+                        href="/measures-and-code"
+                        className="text-lg text-white hover:text-gray-300"
+                    >
+                        Measures and Code
+                    </Link>
+                    <Link
+                        href="/reasearch-topics"
+                        className="text-lg text-white hover:text-gray-300"
+                    >
+                        Research Topics
+                    </Link>
+                </div>
             </div>
-
-            <div className="flex items-center gap-8 md:show lg:hidden">
-                <Image
-                    src={menuBarSvg}
-                    alt="menu"
-                    width={24}
-                    height={24}
-                    className="w-[24px] h-[24px] object-contain"
-                />
-            </div>
-        </div>
-    </motion.nav>
-);
+            <AnimatePresence>
+                {isOpen && <motion.ul className="origin-top text-center mt-1" variants={mobileMenuVariants} initial="initial" animate="animate" exit="exit">
+                    <motion.li className="my-1">
+                        <Link
+                            href="/"
+                            className="text-lg text-white hover:text-gray-300"
+                            onClick={handleMenuClick}
+                        >
+                            Home
+                        </Link>
+                    </motion.li>
+                    <motion.li className="my-1">
+                        <Link
+                            href="/#about-us"
+                            scroll
+                            className="text-lg text-white hover:text-gray-300"
+                            onClick={handleMenuClick}
+                        >
+                            About Us
+                        </Link>
+                    </motion.li>
+                    <motion.li className="my-1">
+                        <Link
+                            href="/contact"
+                            className="text-lg text-white hover:text-gray-300"
+                            onClick={handleMenuClick}
+                        >
+                            Contact
+                        </Link>
+                    </motion.li>
+                    <motion.li className="my-1">
+                        <Link
+                            href="/press"
+                            className="text-lg text-white hover:text-gray-300"
+                            onClick={handleMenuClick}
+                        >
+                            Press
+                        </Link>
+                    </motion.li>
+                    <motion.li className="my-1">
+                        <Link
+                            href="/publications"
+                            className="text-lg text-white hover:text-gray-300"
+                            onClick={handleMenuClick}
+                        >
+                            Publications
+                        </Link>
+                    </motion.li>
+                    <motion.li className="my-1">
+                        <Link
+                            href="/measures-and-code"
+                            className="text-lg text-white hover:text-gray-300"
+                            onClick={handleMenuClick}
+                        >
+                            Measures and Code
+                        </Link>
+                    </motion.li>
+                    <motion.li className="my-1">
+                        <Link
+                            href="/reasearch-topics"
+                            className="text-lg text-white hover:text-gray-300"
+                            onClick={handleMenuClick}
+                        >
+                            Research Topics
+                        </Link>
+                    </motion.li>
+                </motion.ul>}
+            </AnimatePresence>
+        </motion.nav>
+    );
+};
 
 export default Navbar;
