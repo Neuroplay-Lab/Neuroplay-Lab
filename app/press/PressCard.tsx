64c4a83f4a8ca8005b62925ec4e420bd08bc542d
Jack Smith <jackesmith00@gmail.com>
2024-06-03 20:06:42 +0200

    Press page complete

--- app/press/PressCard.tsx
+++ app/press/PressCard.tsx
@@ -1,26 +1,32 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
-import nytImg from "../../public/29Mag-Tip-01-superJumbo.png";
 
 interface Props {
+    title: string;
+    publisher: string;
     href: string;
+    image: string;
+    altTag: string;
 }
 
-const PressCard = () => {
+const PressCard = ({title, publisher, href, image, altTag }: Props) => {
     return (
         <>
-            <Link href={""} className="max-w-sm rounded overflow-hidden shadow-lg bg-slate-400 bg-opacity-10">
-                <Image
-                    src={nytImg}
-                    alt="Test"
-                    className="w-full"
-                    layout="intrinsic"
-                />
-                <div className="px-2 py-4">
-                        <h3 className="text-secondary-white font-bold text-xl mb-1">
-                            How to Get in Sync With Someone
+            <Link href={href} className="max-w-sm rounded overflow-hidden shadow-lg bg-slate-400 bg-opacity-10 hover:bg-opacity-25">
+                <div className="relative h-[120px] md:h-[200px]">
+                    {image && <Image
+                        src={image}
+                        alt={altTag}
+                        className="w-full"
+                        fill
+                        style={{objectFit: "cover"}}
+                    />}
+                </div>
+                <div className="px-2 py-4 relative">
+                        <h3 className="text-white font-bold text-xl mb-1">
+                            {title}
                         </h3>
-                        <h4 className="text-secondary-white">The New York Times</h4>
+                        <h4 className="text-secondary-white">{publisher}</h4>
                 </div>
             </Link>
         </>
--- app/press/page.tsx
+++ app/press/page.tsx
@@ -1,18 +1,66 @@
+import Link from "next/link";
 import { TitleText, TypingText } from "../components";
 import PressCard from "./PressCard";
-
-
+import pressArticles from "./pressArticles.json";
 
 const Press = () => {
     return (
         <>
             <TitleText title={"Press"} textStyles={"text-center mx-4"} />
-            <section className="m-4">
+            <section className="m-4 md:mx-8">
                 <TypingText title={"Articles"} textStyles={"m-4 text-3xl"} />
-                    <div className="grid m-2">
-                        <PressCard />
-                    </div>
+                <div className="grid justify-center m-2 gap-4 md:grid-cols-2 lg:grid-cols-4">
+                    {pressArticles.map(
+                        ({ title, publisher, href, image, imageAlt }) => (
+                            <PressCard
+                                title={title}
+                                publisher={publisher}
+                                href={href}
+                                image={image}
+                                altTag={imageAlt}
+                                key={href}
+                            />
+                        )
+                    )}
+                </div>
             </section>
+            <span className="m-8 w-1/2 mx-auto block border-b border-b-[#b0b0b0]"></span>
+            <section className="m-4 md:mx-8">
+                <TypingText
+                    title={"Seminars"}
+                    textStyles={"mx-4 mb-2 text-3xl"}
+                />
+                <p className="flex-1 font-normal text-[18px] text-[#B0B0B0] leading-[32.4px] mb-4">
+                    Here are a few seminars Gray did for the Autism Research
+                    Institute
+                </p>
+                <div className="grid gap-4 md:grid-cols-2 justify-center">
+                    <iframe
+                        className="max-w-full aspect-video"
+                        width={560}
+                        src="https://www.youtube.com/embed/V-wcTCVh-oc?si=h1jKF6KrD6FsAZDo"
+                        title="YouTube video player"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowFullScreen
+                    ></iframe>
+                    <iframe
+                        className="max-w-full aspect-video"
+                        width={560}
+                        src="https://www.youtube.com/embed/_9ctfGPeEIY?si=v9f0aBHuqGQbscDK"
+                        title="YouTube video player"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowFullScreen
+                    ></iframe>
+                </div>
+            </section>
+                <span className="m-8 w-1/2 mx-auto block border-b border-b-[#b0b0b0]"></span>
+            <p className="text-[18px] text-[#B0B0B0] leading-[32.4px] mx-4 md:mx-8">
+                Also, please check out our{" "}
+                <Link className="underline text-secondary-white hover:text-white font-medium" href="https://www.psychologytoday.com/gb/contributors/liam-cross-phd-and-gray-atherton-phd">
+                    Psychology Today
+                </Link>{" "}
+                blog for some interesting posts!
+            </p>
         </>
     );
 };
