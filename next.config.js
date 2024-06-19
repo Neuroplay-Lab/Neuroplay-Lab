7316b7b1b3af9cadc4a5e1fec94518bdc4625322
Jack Smith <jackesmith00@gmail.com>
2024-06-19 22:37:08 +0200

    Attempt 4

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: "/Neuroplay-Lab",
+    output: "export",
+    reactStrictMode: true,
 };
 
 module.exports = nextConfig;
