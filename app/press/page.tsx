1947276e62dd82aca2f6284bbd7ebdb42cd72a33
Jack Smith <jackesmith00@gmail.com>
2024-06-19 22:27:12 +0200

    Attempt 3 at missing img fix

--- app/press/page.tsx
+++ app/press/page.tsx
@@ -16,7 +16,7 @@ const Press = () => {
                                 title={title}
                                 subtext={publisher}
                                 href={href}
-                                image={image}
+                                image={"/./" + image}
                                 altTag={imageAlt}
                                 key={href}
                             />
