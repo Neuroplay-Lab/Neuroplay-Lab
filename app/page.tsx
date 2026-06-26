c6685d86d7f55fdd69be205195a04b94a986984a
Jack Smith <jackesmith00@gmail.com>
2026-06-26 08:12:52 +0100

    Removed PrimaryResearch section

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,12 @@ const Home = () => (
     <main className="overflow-hidden">
         <div className="relative">
             <Intro />
-            {/* <Hero /> */}
             <RollingWellBanner />
             <div className="gradient-03 -z-10" />
             <div className="relative right-72 -z-10">
                 <div className="gradient-02 z-0" />
             </div>
-            <div className="relative top-[500px]">
-                <div className="gradient-04 z-0" />
-            </div>
             <Explore />
-            <PrimaryResearch />
         </div>
     </main>
 );
